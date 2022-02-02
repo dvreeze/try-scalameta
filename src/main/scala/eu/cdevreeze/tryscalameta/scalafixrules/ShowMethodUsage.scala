ced0a3e0773feabf1870db2e2e68a10ac5b8ee50
Chris de Vreeze <chris.de.vreeze@caiway.net>
2022-02-02 22:36:22 +0100

    Adapted ShowMethodUsage program

--- src/main/scala/eu/cdevreeze/tryscalameta/scalafixrules/ShowMethodUsage.scala
+++ src/main/scala/eu/cdevreeze/tryscalameta/scalafixrules/ShowMethodUsage.scala
@@ -27,8 +27,7 @@ import scala.util.chaining.scalaUtilChainingOps
  * Shows method definitions and where they are used.
  *
  * This "rule" only depends on the Scala standard library and on Scalafix (and therefore Scalameta) and nothing else, so
- * this rule can easily be run from its source path. For example:
- * "scalafix --rules=file:/path/to/ShowMethodUsage.scala"
+ * this rule can easily be run from its source path. For example: "scalafix --rules=file:/path/to/ShowMethodUsage.scala"
  *
  * @author
  *   Chris de Vreeze
@@ -36,33 +35,35 @@ import scala.util.chaining.scalaUtilChainingOps
 class ShowMethodUsage extends SemanticRule("ShowMethodUsage") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    val publicMethodDefs: Seq[Symbol] = doc.tree
+    val publicMethodDefnSymbols: Seq[Symbol] = doc.tree
       .collect { case t: Defn.Def =>
-        t.symbol.info
+        t.symbol.info.ensuring(_.forall(_.isMethod)).ensuring(_.forall(_.isDef))
       }
-      .collect {
-        case Some(info) if info.isPublic =>
-          info.ensuring(_.isDef).symbol
-      }
+      .collect { case Some(info) if info.isPublic => info.symbol }
 
-    val publicMethodCalls: Seq[Symbol] = doc.tree.collect { case t: Term.Apply =>
-      t.fun.symbol
-    }
+    val publicMethodCalls: Seq[Term.Apply] = doc.tree.collect { case t: Term.Apply => t }
+    val publicMethodCallsBySymbol: Map[Symbol, Seq[Term.Apply]] = publicMethodCalls.groupBy(_.fun.symbol)
 
     val fileName: Path = doc.input.asInstanceOf[Input.VirtualFile].path.pipe(Paths.get(_)).getFileName
 
     println()
-    println(s"Method definitions (public) in $fileName")
-    println()
-    publicMethodDefs.foreach(m => println(s"$m\n\tDisplay name: ${m.displayName}\n\tOwner: ${m.owner}"))
+    println(s"Method definitions (public) and their usage in $fileName:")
 
-    println()
-    println(s"Method calls in $fileName")
-    println()
-    publicMethodCalls.foreach(m => println(s"$m\n\tDisplay name: ${m.displayName}\n\tOwner: ${m.owner}"))
+    publicMethodDefnSymbols.foreach { defSymbol =>
+      println()
+      println(s"Symbol: $defSymbol")
+      println(s"\tDisplay name: ${defSymbol.displayName}")
+      println(s"\tOwner: ${defSymbol.owner}")
 
-    // TODO Find usages of those method definitions
-    // TODO Better: summarize method definitions and method calls per source file, and use another program to reason about them
+      val methodCalls: Seq[Term.Apply] = publicMethodCallsBySymbol.getOrElse(defSymbol, Seq.empty)
+
+      methodCalls.foreach { methodCall =>
+        println(s"Method call (in ${methodCall.symbol.owner}):")
+        println(s"\t${methodCall.syntax}")
+      }
+    }
+
+    // TODO Turn this program into something useful
 
     Patch.empty
   }
